7d58599a7c8baa47b2f3c676829d2b24ad34e7e6
moralesdev09 <moralesdev00@gmail.com>
2025-04-09 09:03:20 -0600

    feature:create buttons and store logic for the app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setcount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.countText}>{count}</Text>
+      <View style={styles.pressableContainer}>
+        <Pressable style={styles.pressable} onPress={() => setcount(0)}>
+          <Text>Clear</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.pressable}
+          onPress={() => setcount(count + 1)}
+          onLongPress={() => setcount(0)}
+        >
+          <Text>+1</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  countText: {
+    fontSize: 100,
+    fontWeight: 100,
+  },
+  pressable: {
+    backgroundColor: "#65558F",
+    padding: 20,
+    marginInline: 10,
+    borderRadius: 15,
+    shadowRadius: 4,
+    //IOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.4,
+    //ANDROID
+    elevation: 10,
+  },
+  pressableContainer: {
+    display: "flex",
+    position: "absolute",
+    bottom: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
 });
