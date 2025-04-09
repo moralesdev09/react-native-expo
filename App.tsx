3935dab4151079d73e7ee50af52392685c65c623
moralesdev09 <moralesdev00@gmail.com>
2025-04-09 10:28:00 -0600

    clean: set up develop branch as base branch for reusables components, services etc... of React Native

--- App.tsx
+++ App.tsx
@@ -1,27 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import PressableButton from "./components/PressableButton";
-
+import { StyleSheet, Text, View } from "react-native";
 export default function App() {
-  const [count, setcount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text style={styles.countText}>{count}</Text>
-      <View style={styles.pressableContainer}>
-        <PressableButton
-          label="Clear"
-          onPress={() => setcount(0)}
-        ></PressableButton>
-        <PressableButton
-          styles={styles.pressable}
-          label="+1"
-          onPress={() => setcount(count + 1)}
-          onLongPress={() => {
-            setcount(0);
-          }}
-        ></PressableButton>
-      </View>
+      <Text>Hola Mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -33,20 +15,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  countText: {
-    fontSize: 100,
-    fontWeight: 100,
-  },
-  pressableContainer: {
-    display: "flex",
-    position: "absolute",
-    bottom: 30,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-  },
-  pressable: {
-    backgroundColor: "green",
   },
 });
